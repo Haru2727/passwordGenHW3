bf014a09423cb6e24483c26f55419b6147b6831c
Arash Saeedi <saeediarash84@gmail.com>
2020-12-29 22:47:43 -0500

    password generator fucntion added along with a loop for returning a password with chosen character types.

--- script.js
+++ script.js
@@ -42,3 +42,56 @@ function generatePassword() {
         alert("You must select at least one character type!");
     }
 }
+console.log(generatePassword)
+
+// setting a minimum count for all variables under length.
+var minCount = 0;
+
+// setting empty minimums for same variables. 
+var minUpperCase = "";
+var minLowerCase = "";
+var minNumbers = "";
+var minSpecial = "";
+
+// Password Generator functions 
+var genArray = {
+    getUppercase: function(){
+        return String.fromCharCode(Math.floor(Math.random()* 26 + 65));
+    },
+    getLowerCase: function(){
+        return String.fromCharCode(Math.floor(Math.random() * 28 + 97));
+    },
+    getNumbers: function(){
+        return String.fromCharCode(Math.floor(Math.random() * 10 + 48));
+    },
+    getSpecialCharacters: function(){
+        return String.fromCharCode[Math.floor(Math.random() * specialCharacters.length)]
+    },
+};
+
+// Using selected password criteria and empty minimums from above to input into password
+if (upperCase === true){
+    minUpperCase = genArray.getUppercase();
+    minCount++;
+}
+if (lowerCase === true){
+    minLowerCase = genArray.getLowerCase();
+    minCount++;
+}
+if (numbers === true){
+    minNumbers = genArray.getNumbers();
+    minCount++;
+}
+if (special === true){
+    minSpecail = genArray.getSpecialCharacters();
+    minCount++;
+}
+
+// setting an empty string for a random character loop to be used 
+var randomPassGen = "";
+
+for (let i = 0; i <parseInt(passwordLength)-minCount); i++){
+    var randomNum = Math.floor(Math.random() * 4);
+    randomPassGen += randomNum;
+}
+
